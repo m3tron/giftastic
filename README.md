b52799b6dcfa78d1dedeb43b57fa2c6a601f6adb
Sunny Heyar <sunny.heyar@gmail.com>
2019-03-14 01:15:03 -0400

    completed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# giftastic
+# Superhero Gifs
+
+See gifs of your favorite superhero.
+
+Enter their name and click KaPow.
+
+It will create a button with the name of your superhero.
+
+Click the button to generate 10 gifs.
+
+Click the gif to toggle animation.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,21 @@
-var topics = ["Batman", "Superman"];
+//array of superheroes
+var topics = ["BATMAN", "SUPERMAN", "HULK", "SPIDER MAN"];
 
+//create buttons based on array of superheroes
 function renderButtons() {
   $("#buttons-view").empty();
   for (i = 0; i < topics.length; i++) {
     var a = $("<button>");
-    a.addClass("animals");
+    a.addClass("superheroes");
     a.attr("data-name", topics[i]);
     a.text(topics[i]);
     $("#buttons-view").append(a);
   }
 }
-function animateGif() {}
+
+//retrieve and display gifs of superheroes
 function displayGifs() {
-  query = "Bruce+wayne"; //$(this).attr("data-name");
+  query = $(this).attr("data-name");
 
   queryUrl =
     "https://api.giphy.com/v1/gifs/search?q=" +
@@ -23,22 +26,66 @@ function displayGifs() {
     url: queryUrl,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
-    var gifDiv = $("<div class='gifs'>");
+    var gifDiv = $("<div class='gifClass'>");
+
+    //clears gifs already on page
+    $(".gifClass").empty();
+
     for (i = 0; i < response.data.length; i++) {
+      //URL of still image
       var gifStillURL = response.data[i].images.original_still.url;
+      //URL of animated image
       var gifAnimatedURL = response.data[i].images.original.url;
-      var gifDisplay = $("<img>").attr("src", gifStillURL);
+      //URL of rating
       var gifRating = response.data[i].rating.toUpperCase();
+
+      //initial still gif
+      var gifDisplay = $(
+        "<img class='gifs' data-still=" +
+          gifStillURL +
+          " data-animate=" +
+          gifAnimatedURL +
+          " data-state='still'>"
+      ).attr("src", gifStillURL);
+
+      //displays gifs
       gifDiv.append(gifDisplay);
-      gifDiv.append("<p id='gifRating'>Rating: " + gifRating + "</p>");
-      $("gifDisplay").on("click", function() {
-        var gifDisplay = $("<img>").attr("src", gifAnimatedURL);
-      });
+      //displays rating under gif image
+      gifDiv.append("<p id='gifRating'>Rating: " + gifRating + "</p><hr>");
     }
-    $("#buttons-view").append(gifDiv);
+    //makes sure buttons and search bar remain on top of page
+    $("#remainOnTop").append(gifDiv);
   });
 }
 
+//function that alter the state of the gif on click (from still to animated and animated to still)
+$(document).on("click", ".gifs", function() {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
+
+//funtion that adds a button of the superhero entered in search form
+$("#add-superhero").on("click", function(event) {
+  event.preventDefault();
+  //will always display in uppercase on button to insure consistency
+  var superhero = $("#superhero-input")
+    .val()
+    .trim()
+    .toUpperCase();
+  //adds superhero to array
+  topics.push(superhero);
+  renderButtons();
+});
+
+//displays gifs on clicking the superhero button
+$(document).on("click", ".superheroes", displayGifs);
+
+//displays initial buttons
 renderButtons();
-displayGifs();
